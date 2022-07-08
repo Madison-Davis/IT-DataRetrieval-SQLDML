0077549d17be522dbeb12810e8c34a0dffab8126
Madison <52668142+Madison-Davis@users.noreply.github.com>
2022-07-08 08:34:30 -0500

    Add files via upload

---
+++ Personal-IT-DataRetrieval-DML.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE stockMarketDatabase;
+USE stockMarketDatabase;
+SET SQL_SAFE_UPDATES = 0;
+INSERT INTO stockMarket VALUES ("2013-02-08", 14, 14.5, 13.5, 14, 1245100, "ZION");
+UPDATE stockMarket SET Company = "LEO" WHERE Company = "ZION";
+DELETE FROM stockMarket WHERE Company = "LEO";
+SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM stockMarket;
